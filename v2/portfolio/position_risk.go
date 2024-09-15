c7f80184631ee14fddf79a8b6aec5695d241d88c
jzhu-eth <zuriel.blockchain@gmail.com>
2024-09-15 15:08:05 +0800

    support um positions

--- v2/portfolio/position_risk.go
+++ v2/portfolio/position_risk.go
@@ -4,16 +4,29 @@ import (
 	"context"
 
 	"github.com/photon-storage/go-binance/v2/delivery"
+	"github.com/photon-storage/go-binance/v2/futures"
 )
 
-type GetPositionRiskService struct {
+type GetPositionRiskServiceCM struct {
 	ds *delivery.GetPositionRiskService
 }
 
-func (s *GetPositionRiskService) Do(
+func (s *GetPositionRiskServiceCM) Do(
 	ctx context.Context,
 	opts ...delivery.RequestOption,
 ) ([]*delivery.PositionRisk, error) {
 	opts = append(opts, delivery.WithEndpoint("/papi/v1/cm/positionRisk"))
 	return s.ds.Do(ctx, opts...)
+}
+
+type GetPositionRiskServiceUM struct {
+	fs *futures.GetPositionRiskService
+}
+
+func (s *GetPositionRiskServiceUM) Do(
+	ctx context.Context,
+	opts ...futures.RequestOption,
+) ([]*futures.PositionRisk, error) {
+	opts = append(opts, futures.WithEndpoint("/papi/v1/um/positionRisk"))
+	return s.fs.Do(ctx, opts...)
 }
